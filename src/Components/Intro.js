8092a6bbb0365e90bd2007b3d845368ec0e38ca7
Boze-noob <bozeskoko16@gmail.com>
2022-05-31 18:45:42 +0200

    intro sound and animation

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,21 +1,55 @@
 import '../Css/Intro.css';
 import Button from '../Components/Button.js';
 import { motion } from 'framer-motion';
-import useSound from 'use-sound';
 import mySound from '../Audio/background_sound.mp3'
+import React, {useState, useRef } from 'react';
+
+import animationData from '../Animations/android_jetpack.json';
+import Lottie from 'lottie-react-web';
 
 
 function Intro() {
   const titleTxt = 'Hi! \nI am Bože, \nmobile developer.';
-  const [playSound] = useSound(mySound)
+  const [musicFlag, switchMusic] = useState(true);
+
+  let audio = useRef(new Audio(mySound));
+  audio.current.addEventListener('ended', function () {
+    audio.current.currentTime = 0;
+    audio.current.play();
+  }, false);
+
+
+  const start = () => {
+    audio.current.play();
+    switchMusic(false);
+  }
+  const pause = () => {
+    audio.current.pause();
+    switchMusic(true);
+  }
   
   return (
     <div className="Intro">
-      <div id='soundBtn'>
-      <button onClick={playSound}>
-       Play Sound
-    </button>
+      <div id='arrow'>
+      <h2 id='arrow_desc'>Turn on the music!</h2>
+      <img id='arrow_image' src='/images/arrow.jpg' />
       </div>
+      <div id='music'>
+      <img id='sound_image' src='/images/sound.jpg' />
+      <motion.button whileHover={{ scale: 1.45}} transition={{duration: 0.4 }} onClick={musicFlag ? start : pause}>
+       {musicFlag ? 'Play music' : 'Stop music'}
+    </motion.button>
+      </div>
+      <div className='JetpackAnimation'>
+      <Lottie 
+      options={{
+        animationData: animationData,
+        loop: true,
+        autoplay: false,
+      }} />
+      </div>
+      
+
       <div id='description'>
       {
         titleTxt.split('\n').map(function(txt){
