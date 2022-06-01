4d7b9cba93275cdbd6b3131aff9e1aa15ac9c986
Boze-noob <bozeskoko16@gmail.com>
2022-06-01 17:12:22 +0200

    navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Sidebar from "./Components/Sidebar";
 import Intro from "./Components/Intro";
+import AboutMe from "./Components/AboutMe";
+import Projects from "./Components/Projects";
+import MySkills from "./Components/MySkills";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Sidebar/>
       <Intro />
+      <MySkills />
+      <Projects />
+      <AboutMe />
+      <Contact />
     </div>
   );
 }
