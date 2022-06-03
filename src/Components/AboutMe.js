3a02714ea53f0c7aae0bf489b4afe07af8ce8f12
Boze-noob <bozeskoko16@gmail.com>
2022-06-03 19:22:47 +0200

    About me UI

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,12 +1,31 @@
-import '../Css/AboutMe.css'
+import '../Css/AboutMe.css';
+import {motion} from 'framer-motion';
+import Lottie from 'react-lottie';
+import aboutMeAnim from '../Animations/about_me.json';
 
 
 function AboutMe() {
   return (
     <div className="AboutMe" id='aboutMeId'>
-      
+
+<div className='Animation'>
+      <Lottie 
+      options={{
+        animationData: aboutMeAnim,
+        loop: true,
+        autoplay: false,
+      }} />
+      </div>
+
+       <div className='Title'>
+      <motion.p transition={{ type: 'tween', duration: 0.3 }}
+         whileHover={{ scale: 1.1, originX: -0.1, originY: -0.4, color: '#FD2155', }}>
+           About me</motion.p>
     </div>
-  );
-}
+    <div className='Description'>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      </div>
+    </div>
+    )}
 
 export default AboutMe;
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,18 @@
-import '../Css/Contact.css'
+import '../Css/Contact.css';
+import {motion} from 'framer-motion';
 
 
 function Contact() {
   return (
     <div className="Contact" id='contactId'>
-      <p id='contactPId'>This is your p id</p>
+      <div className='Title'>
+      <motion.p transition={{ type: 'tween', duration: 0.3 }}
+         whileHover={{ scale: 1.1, originX: -0.1, originY: -0.4, color: '#FD2155', }}>
+           Contact me</motion.p>
+    </div>
+    <div className='Description'>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.</p>
+      </div>
     </div>
   );
 }
