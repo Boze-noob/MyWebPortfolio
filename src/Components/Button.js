529fc750c6f4c65064de519d81bd01f909c7294b
Boze-noob <bozeskoko16@gmail.com>
2022-06-05 15:49:43 +0200

    button refactored

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,9 +1,18 @@
-import {Button} from "@mui/material"
-import "../Css/Button.css"
+import {Button} from "@mui/material";
+import "../Css/Button.css";
 
 function ButtonComp(buttonTxt) {
+
+
+  //Since we just have one button in the app its okay to call this function this way, otherwise we would use callback function to
+  //handle click event
+   const scrollToContact = () => {
+    const anchor = document.getElementById('contactId');
+    anchor.scrollIntoView({behavior:'smooth', block: 'center'});
+  }
+
   return (
-    <Button variant="outlined" size='large'  style={{
+    <Button variant="outlined" size='large' onClick={scrollToContact}  style={{
         borderWidth: 2,
         borderColor: "#21b6ae",
         fontFamily: "Lucida Sans",
